c4c2e7217e3988a07f8883c58179509e7dc3973e
kevinche75 <kevinche75@gmail.com>
2020-05-07 12:13:36 +0400

    renamed

--- Part_3/P3210_DBelyakov_1628.cpp
+++ Part_3/P3210_DBelyakov_1628.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <utility>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
@@ -25,63 +24,65 @@ int main() {
     int m, n, k;
     int res = 0;
 
+    int p_i = 0, p_k = 0;
+
     cin >> m >> n >> k;
 
-    vector<pair<int, int>> point;
-    vector<pair<int, int>> single_point;
+    pair<int, int> point[180000];
+    pair<int, int> single_point[180000];
 
-    for (int i = 0; i < k; ++i) {
+    for (int i = 0; i < k; i++) {
         pair<int, int> p;
         cin >> p.first >> p.second;
         p.first--;
         p.second--;
-        point.push_back(p);
+        point[p_i++] = p;
     }
 
     for (int i = 0; i < m; i++) {
         pair<int, int> p = {i, -1};
         pair<int, int> p2 = {i, n};
-        point.push_back(p2);
-        point.push_back(p);
+        point[p_i++] = p2;
+        point[p_i++] = p;
     }
 
     for (int i = 0; i < n; i++) {
         pair<int, int> p = {-1, i};
         pair<int, int> p2 = {m, i};
-        point.push_back(p2);
-        point.push_back(p);
+        point[p_i++] = p2;
+        point[p_i++] = p;
     }
 
-    sort(point.begin(), point.end(), comp1);
+    sort(&point[0], &point[p_i], comp1);
 
-    for (int i = 0; i < point.size() - 1; i++) {
+    for (int i = 0; i < p_i; i++) {
         if (point[i].first == point[i + 1].first) {
             if (point[i + 1].second - point[i].second - 1 > 1) {
                 res++;
             } else if (point[i + 1].second - point[i].second - 1 == 1) {
                 pair<int, int> p(point[i].first, point[i].second + 1);
-                single_point.push_back(p);
+                single_point[p_k++] = p;
             }
         }
     }
 
-    sort(point.begin(), point.end(), comp2);
+    sort(&point[0], &point[p_i], comp2);
 
-    for (int i = 0; i < point.size() - 1; i++) {
+    for (int i = 0; i < p_i; i++) {
         if (point[i].second == point[i + 1].second) {
             if (point[i + 1].first - point[i].first - 1 > 1) {
                 res++;
             } else if (point[i + 1].first - point[i].first - 1 == 1) {
                 pair<int, int> p(point[i].first + 1, point[i].second);
-                single_point.push_back(p);
+                single_point[p_k++] = p;
             }
         }
     }
 
-    sort(single_point.begin(), single_point.end(), comp1);
+    sort(&single_point[0], &single_point[p_k+1], comp1);
 
-    if (single_point.size() > 1) {
-        for (int i = 0; i < single_point.size() - 1; i++) {
+    if (p_k + 1 > 1) {
+        for (int i = 0; i < p_k; i++) {
             if (single_point[i] == single_point[i + 1]) {
                 res++;
                 i++;
